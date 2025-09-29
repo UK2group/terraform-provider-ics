4d8a9be898324bbe40353a28c2d4c41bbf755450
Jason Yates <jason.yates79@thehutgroup.com>
2025-09-29 21:10:24 +0100

    Fixing liter failing for builds

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -2,18 +2,7 @@ package provider
 
 import (
 	"testing"
-
-	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 )
-
-// testAccProtoV6ProviderFactories are used to instantiate a provider during
-// acceptance testing. The factory function will be invoked for every Terraform
-// CLI command executed to create a provider server to which the CLI can
-// reattach.
-var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
-	"ics": providerserver.NewProtocol6WithError(New("test")()),
-}
 
 func TestAccProvider(t *testing.T) {
 	// This test simply verifies that the provider can be instantiated
